f94ca34291932a99647dbf2f9986a390e960fdce
Adnan Siddiqui <msiddiqui@spartaglobal.com>
2023-05-09 17:55:09 +0100

    Added test comments

--- src/main/java/EmployeeFactory.java
+++ src/main/java/EmployeeFactory.java
@@ -6,6 +6,7 @@ public class EmployeeFactory {
     // 1 <= numEmployees <= 1000
     // may throw IOExceptions, which need to be dealt with in the client code
     // employees.csv should be in the src/main/resources folder in the project
+    // Test comments
     public static String[] getEmployees(int numEmployees) throws IOException {
         if (numEmployees < 1 || numEmployees > 1000)
             throw new IllegalArgumentException("Argument 'numEmployees' must be between 1 and 1000");
