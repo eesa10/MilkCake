7932ddb5de71377adce1119ec11ed5a124044cad
Mohamed Yusuf <myusuf@spartaglobal.com>
2023-05-10 15:45:40 +0100

    Pushing the project to dev branch

---
+++ src/Main/com/sparta/mc/App.java
@@ -0,0 +1,15 @@
+package Main.com.sparta.mc;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            String[] employees = EmployeeFactory.getEmployees(6);
+            System.out.println(Arrays.toString(employees));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
