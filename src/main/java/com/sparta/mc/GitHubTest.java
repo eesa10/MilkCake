047340a58f4db4f09aa00e3669d6e275944381a1
Eesa Mannan <emannan@spartaglobal.com>
2023-05-11 13:36:04 +0100

    new class

--- src/main/java/com/sparta/mc/GitHubTest.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.mc;
-
-public class GitHubTest {
-    public static void main(String[] args) {
-        System.out.println(123);
-    }
-}
--- src/main/java/com/sparta/mc/NewClass.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.mc;
-
-public class NewClass {
-    public static void main(String[] args) {
-        System.out.println(1234);
-    }
-}
