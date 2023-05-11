15721e50a127e8f5ff99493e48ffab883ad07fc9
Eesa Mannan <emannan@spartaglobal.com>
2023-05-11 14:00:36 +0100

    employees csv file added (new one)

--- src/main/java/com/sparta/mc/EmployeeFactory.java
+++ src/main/java/com/sparta/mc/EmployeeFactory.java
@@ -7,14 +7,14 @@ public class EmployeeFactory {
     // returns an array containing numEmployees Strings, each representing an Employee as a row from the CSV file
     // 1 <= numEmployees <= 1000
     // may throw IOExceptions, which need to be dealt with in the client code
-    // employees.csv should be in the src/main/resources folder in the project
+    // employeess.csv should be in the src/main/resources folder in the project
     // Test comments
     public static String[] getEmployees(int numEmployees) throws IOException {
         if (numEmployees < 1 || numEmployees > 1000)
             throw new IllegalArgumentException("Argument 'numEmployees' must be between 1 and 1000");
         String employeeLine;
         List<String> result = new ArrayList<>();
-        BufferedReader f = new BufferedReader(new FileReader("src/main/resources/employees.csv"));
+        BufferedReader f = new BufferedReader(new FileReader("src/main/resources/employeess.csv"));
         // read all the records from the file and add them to the list
         while ((employeeLine = f.readLine()) != null)
             result.add(employeeLine);
