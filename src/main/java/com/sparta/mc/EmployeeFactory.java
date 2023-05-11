58903cd9facc07044f242491ad4632ffccfa66ef
Shahana Shareef <shahanashareef1991@gmail.com>
2023-05-11 13:28:22 +0100

    new

--- src/main/java/com/sparta/mc/EmployeeFactory.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.mc;
-
-public class EmployeeFactory {
-    public static void main(String[] args) {
-        System.out.println("This is for testing github");
-    }
-}
--- src/main/java/com/sparta/mc/TestingGitHubPush.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.mc;
-
-public class TestingGitHubPush {
-    public static void main(String[] args) {
-        System.out.println(123);
-    }
-}
