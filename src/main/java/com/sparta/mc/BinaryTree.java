8c7419cb08eabdb9cf88f6b7c5e62b6f13a7a7cd
Eesa Mannan <emannan@spartaglobal.com>
2023-05-12 14:34:49 +0100

    Node Class nested within BinaryTree

--- src/main/java/com/sparta/mc/BinaryTree.java
+++ src/main/java/com/sparta/mc/BinaryTree.java
@@ -1,5 +1,38 @@
 package com.sparta.mc;
 class BinaryTree {
+    class Node {
+        private Employee employee;
+        private Node left;
+        private Node right;
+
+        Node(Employee employee){
+            this.employee=employee;
+        }
+
+        public Employee getEmployee() {
+            return employee;
+        }
+
+        public void setEmployee(Employee employee) {
+            this.employee = employee;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
     private Node root;
     public void insert(Employee employee) {
         root = insertNode(root, employee);
